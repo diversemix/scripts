a36aa0e12b33f3cce5163f668a217711480adfb0
diversemix <diversemix@gmail.com>
2015-07-26 18:37:39 +0100

    modified for all folders

--- sort_photos.sh
+++ sort_photos.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
-thisyear=$(pwd | cut -d '-' -f2)
-echo $thisyear
+: ${PHOTO_DEST?"Need a destination for the photos"}
 
-find . -type f  -print0| while read -d '' -r file 
+SEARCH_DIR=$1
+
+if [ -z "$SEARCH_DIR" ]; then
+    echo "Need a argument for the search folder for the photos"
+    exit 1
+fi  
+
+
+FIND=".*/.*\.\(mov\|MOV\|mp4\|MP4\|avi\|AVI\|jpg\|JPG\)"
+
+echo Searching $SEARCH_DIR
+find $SEARCH_DIR -type f -regex $FIND -print0| while read -d '' -r file 
 do 
-	line=$(ls -l  "$file") 
+	line=$(ls -l --time-style="+%b %d %Y" "$file") 
 	month=$(echo $line | cut -d ' ' -f6)
 	year=$(echo $line | cut -d ' ' -f8)
-#	echo "<$thisyear> <$year>"
 
-	if [  "$thisyear" != "$year" ] 
-	then
-		echo mv $file "../Photos-$year/$file"
-	fi
+	folder=$PHOTO_DEST/P_$year/$month
+	mkdir -p $folder
+	cp -n "$file" $folder
+	echo -n .
 done
+echo
+echo Done!
